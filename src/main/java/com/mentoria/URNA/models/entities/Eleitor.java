1f63570e6a97d742be6ad4b5a2b708547a46e56c
caio.neves <caio.neves@ifood.com.br>
2023-08-02 17:15:56 -0300

    Configuring database, creating some entities and adding depedencies

---
+++ src/main/java/com/mentoria/URNA/models/entities/Eleitor.java
@@ -0,0 +1,56 @@
+package com.mentoria.URNA.models.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TABELA_ELEITOR")
+public class Eleitor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String dataDeNascimento;
+    private String cpf;
+    private String rg;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(String dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
